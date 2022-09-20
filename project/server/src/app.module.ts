3e4c7e697edd54023110869de89485a83e136842
ThamTC <truongcongtham91@gmail.com>
2022-09-20 23:03:12 +0700

    add serve static path, edit path to build angular

--- project/server/src/app.module.ts
+++ project/server/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost:27017/mydatabase')],
+  imports: [UsersModule, MongooseModule.forRoot('mongodb://localhost:27017/mydatabase'), ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'public')
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
